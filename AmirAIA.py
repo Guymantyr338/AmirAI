b59c13eb3d602ccb1954cb4cb504bbe5c33d385b
Guymantyr338 <121900951+Guymantyr338@users.noreply.github.com>
2024-04-15 18:22:32 +0200

    amirai
    Amirai

---
+++ AmirAIA.py
@@ -0,0 +1,19 @@
+# AmirAI
+user_input = input("Say ‘AmirAI.start’ to start the AI: ").lower()
+
+if user_input == "amirai.start":
+    print("AmirAI started")
+
+user_input = input("hi im amirAI: ")
+if user_input.lower() == "whats your name":
+    print("My name is amirali samizadeh.")
+if user_input.lower() == "whats your age":
+    print("My age is 9 .")
+if user_input.lower() == "whats 1+1":
+    print("1+1 is 2 😃😃😃")
+if user_input.lower() == "whats your yt":
+    print("I don't have a YouTube channel as an AI.")
+
+else:
+    print("i need to be updated to answer your question")
+    import AmirAI.py
